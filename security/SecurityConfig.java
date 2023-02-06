0f21d06d7e7679ceed49b099f0b8fda8582b52ab
psegarrac <94398207+psegarrac@users.noreply.github.com>
2023-02-06 10:48:17 +0100

    Security Config

---
+++ security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.paracasa.spring.app.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/auth/**","/css/**",
+                        "/js/**","/images/**").permitAll().anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/auth/login").defaultSuccessUrl("/",
+                        true).failureUrl("/auth/login?error=true")
+                .loginProcessingUrl("/auth/login-post").permitAll()
+                .and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/");
+    }
+}
